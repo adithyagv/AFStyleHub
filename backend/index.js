4dbb72c8b4c9350d534fa86609c4a3dee6c34b72
adithyagv <aadithyagoudhaman@gmail.com>
2024-11-05 08:52:57 +0530

    Updated backend links

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'https://af-style-hub-backend.vercel.app/'}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
