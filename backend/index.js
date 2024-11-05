a5c803802a657aaf41cedc08807443fefad86d8f
adithyagv <aadithyagoudhaman@gmail.com>
2024-11-05 10:45:17 +0530

    Updated backend API links

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://af-stylehub-backend.vercel.app' // Update this with your frontend URL
+}));
+
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
@@ -16,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/fashionDB', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
@@ -24,6 +27,7 @@ mongoose.connect('mongodb://localhost:27017/fashionDB', {
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error);
 });
+
 
 // User Schema and Model
 const userSchema = new mongoose.Schema({
